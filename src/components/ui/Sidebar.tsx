d36ebb858d6b5b2aa6bd2b5f14cf7b6b23b8b183
Falekin7 <falekinldg@gmail.com>
2025-08-30 03:35:23 -0300

    add a lot of changes xd

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -8,9 +8,13 @@ import {
   RiSettings4Line,
 } from 'react-icons/ri';
 
-const Sidebar = () => {
+const Sidebar = ({ showMenu }) => {
   return (
-    <div className="bg-black text-white fixed left-0 top-0 w-24 h-full flex flex-col justify-between">
+    <div
+      className={`bg-black text-white fixed lg:left-0 top-0 w-22 h-full flex flex-col justify-between py-2 rounded-tr-xl rounded-br-xl z-50 transition-all ${
+        showMenu ? 'left-0' : '-left-full'
+      }`}
+    >
       <div>
         <ul className="pl-3">
           {/* LOGO */}
@@ -19,49 +23,49 @@ const Sidebar = () => {
           </li>
 
           {/* NAV BUTTONS */}
-          <li className="bg-zinc-900 p-4 rounded-tl-xl rounded-bl-xl">
-            <a href="#" className="bg-red-400 p-3 flex justify-center rounded-xl">
+          <li className="bg-zinc-900 p-2 rounded-tl-xl rounded-bl-xl">
+            <a href="#" className="bg-red-400 p-4 flex justify-center rounded-xl">
               <RiHome6Line className="text-xl" />
             </a>
           </li>
 
-          <li className="hover:bg-zinc-900 p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+          <li className="hover:bg-zinc-900 p-2 rounded-tl-xl rounded-bl-xl group transition-colors">
             <a
               href="#"
-              className="group-hover:bg-red-400 group-hover:text-white text-red-400 p-3 flex justify-center rounded-xl transition-colors"
+              className="group-hover:bg-red-400 group-hover:text-white text-red-400 p-4 flex justify-center rounded-xl transition-colors"
             >
               <RiPercentLine className="text-xl" />
             </a>
           </li>
 
-          <li className="hover:bg-zinc-900 p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+          <li className="hover:bg-zinc-900 p-2 rounded-tl-xl rounded-bl-xl group transition-colors">
             <a
               href="#"
-              className="group-hover:bg-red-400 group-hover:text-white text-red-400 p-3 flex justify-center rounded-xl transition-colors"
+              className="group-hover:bg-red-400 group-hover:text-white text-red-400 p-4 flex justify-center rounded-xl transition-colors"
             >
               <RiPieChartLine className="text-xl" />
             </a>
           </li>
-          <li className="hover:bg-zinc-900 p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+          <li className="hover:bg-zinc-900 p-2 rounded-tl-xl rounded-bl-xl group transition-colors">
             <a
               href="#"
-              className="group-hover:bg-red-400 group-hover:text-white text-red-400 p-3 flex justify-center rounded-xl transition-colors"
+              className="group-hover:bg-red-400 group-hover:text-white text-red-400 p-4 flex justify-center rounded-xl transition-colors"
             >
               <RiMailLine className="text-xl" />
             </a>
           </li>
-          <li className="hover:bg-zinc-900 p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+          <li className="hover:bg-zinc-900 p-2 rounded-tl-xl rounded-bl-xl group transition-colors">
             <a
               href="#"
-              className="group-hover:bg-red-400 group-hover:text-white text-red-400 p-3 flex justify-center rounded-xl transition-colors"
+              className="group-hover:bg-red-400 group-hover:text-white text-red-400 p-4 flex justify-center rounded-xl transition-colors"
             >
               <RiNotification3Line className="text-xl" />
             </a>
           </li>
-          <li className="hover:bg-zinc-900 p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+          <li className="hover:bg-zinc-900 p-2 rounded-tl-xl rounded-bl-xl group transition-colors">
             <a
               href="#"
-              className="group-hover:bg-red-400 group-hover:text-white text-red-400 p-3 flex justify-center rounded-xl transition-colors"
+              className="group-hover:bg-red-400 group-hover:text-white text-red-400 p-4 flex justify-center rounded-xl transition-colors"
             >
               <RiSettings4Line className="text-xl" />
             </a>
@@ -71,10 +75,10 @@ const Sidebar = () => {
 
       <div>
         <ul className="pl-3">
-          <li className="hover:bg-zinc-900 p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+          <li className="hover:bg-zinc-900 p-2 rounded-tl-xl rounded-bl-xl group transition-colors">
             <a
               href="#"
-              className="group-hover:bg-red-400 group-hover:text-white text-red-400 p-3 flex justify-center rounded-xl transition-colors"
+              className="group-hover:bg-red-400 group-hover:text-white text-red-400 p-4 flex justify-center rounded-xl transition-colors"
             >
               <RiLogoutCircleLine className="text-xl" />
             </a>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,7 @@
-import { createBrowserRouter, redirect } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import PublicLayout from '../layouts/PublicLayout';
 import PrivateLayout from '../layouts/PrivateLayout';
 import App from '../App';
-
-// Función para verificar la autenticación
-const checkAuth = () => {
-  // Simulación: en una app real, esto podría ser una llamada a una API
-  const token = localStorage.getItem('jwt');
-  if (!token) {
-    // Si no hay token, redirige al login
-    throw redirect('/');
-  }
-  return token;
-};
 
 const router = createBrowserRouter([
   {
@@ -28,7 +17,7 @@ const router = createBrowserRouter([
   {
     path: '/dashboard',
     element: <PrivateLayout />,
-    loader: checkAuth,
+
     children: [
       {
         index: true,
